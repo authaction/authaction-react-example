464929de39b726ebd7c60fd4b8b660a0d051c939
chandhravadanan <chandhravadanan.ravichandran@agreena.com>
2024-07-07 06:39:14 +0200

    chore: authaction react sample

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useAuth } from "react-oidc-context";
 
 function App() {
+  const auth = useAuth();
+
+  const handleLogin = () => {
+    void auth.signinRedirect();
+  };
+
+  const handleLogout = () => {
+    void auth.signoutRedirect();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {auth.user?.profile?.email ? (
+          <>
+            <p>Welcome, {auth.user.profile.email}</p>
+            <button className="App-link" onClick={handleLogout}>
+              Logout
+            </button>
+          </>
+        ) : (
+          <button className="App-link" onClick={handleLogin}>
+            Login
+          </button>
+        )}
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import AppConfig from "./config.json";
+import { AuthProvider } from "react-oidc-context";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const oidcConfig = {
+  authority: `https://${AppConfig.tenantDomain}`,
+  client_id: AppConfig.clientId,
+  redirect_uri: AppConfig.redirectUri,
+  post_logout_redirect_uri: AppConfig.logoutRedirectUri,
+  onSigninCallback: () => {
+    window.history.replaceState({}, document.title, window.location.pathname);
+  },
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider {...oidcConfig}>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
 
